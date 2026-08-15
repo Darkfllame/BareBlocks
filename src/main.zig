1562c6d497de6c3f02301ff5d98dd323fbf3eab7
Darkfllame <clement.dupuis69@gmail.com>
2026-08-15 19:24:26 +0200

    CountingAllocator, serial demo for benchmarks and stuff idk

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,63 @@
 const std = @import("std");
-const net = @import("net");
+const utils = @import("utils");
+
+const serial = utils.serial;
 
 pub fn main(init: std.process.Init) !void {
-    _ = init;
+    var ca = utils.CountingAllocator.init(init.gpa, true);
+    defer ca.deinit();
+
+    const gpa = ca.allocator();
+
+    const nbt_data = blk: {
+        var aw = std.Io.Writer.Allocating.init(gpa);
+        defer aw.deinit();
+
+        var nbt_w: serial.nbt.Writer = undefined;
+        nbt_w.init(gpa, &aw.writer);
+        defer nbt_w.deinit();
+
+        try nbt_w.beginArray(null);
+        {
+            try nbt_w.beginCompound();
+            {
+                try nbt_w.setFieldName("a");
+                try nbt_w.beginArray(null);
+                {
+                    try nbt_w.beginCompound();
+                    {
+                        try nbt_w.setFieldName("");
+                        try nbt_w.beginArray(null);
+                        try nbt_w.endArray();
+                    }
+                    try nbt_w.endCompound();
+                }
+                try nbt_w.endArray();
+            }
+            try nbt_w.endCompound();
+        }
+        try nbt_w.endArray();
+
+        break :blk try aw.toOwnedSlice();
+    };
+    defer gpa.free(nbt_data);
+    var nbt_input = std.Io.Reader.fixed(nbt_data);
+    std.log.info("Largest allocation: {f}", .{ca});
+
+    std.debug.dumpHex(nbt_data);
+
+    var stdout_buffer: [64]u8 = undefined;
+    var stdout = std.Io.File.stdout().writer(init.io, &stdout_buffer);
+
+    var nbt_r: serial.nbt.Reader = undefined;
+    nbt_r.init(gpa, &nbt_input, false);
+    defer nbt_r.deinit();
+
+    var json_w: serial.json.Writer = undefined;
+    json_w.init(gpa, &stdout.interface, .{ .whitespace = .indent_2 });
+    defer json_w.deinit();
+
+    try nbt_r.mapr.mapToWriter(256, &json_w.mapw);
+
+    try stdout.interface.flush();
 }
---
+++ src/utils/CountingAllocator.zig
@@ -0,0 +1,130 @@
+const CountingAllocator = @This();
+const std = @import("std");
+
+const Allocator = std.mem.Allocator;
+const Alignment = std.mem.Alignment;
+
+const vtable = Allocator.VTable{
+    .alloc = &alloc,
+    .resize = &resize,
+    .remap = &remap,
+    .free = &free,
+};
+
+fn findBlock(self: *CountingAllocator, address: usize) ?*?[]u8 {
+    for (self.allocs.items) |*block| {
+        if (address == 0 and block.* == null) return block;
+        if (block.* == null) continue;
+        if (@intFromPtr(block.*.?.ptr) == address) return block;
+    }
+    return null;
+}
+
+fn alloc(ud: *anyopaque, size: usize, al: Alignment, ret_addr: usize) ?[*]u8 {
+    const self: *CountingAllocator = @ptrCast(@alignCast(ud));
+    if (self.count_largest) self.allocs.ensureUnusedCapacity(self.parent, 1) catch return null;
+    const ret = self.parent.rawAlloc(size, al, ret_addr) orelse return null;
+    self.total += size;
+    if (self.count_largest) {
+        if (self.findBlock(0)) |bptr| {
+            bptr.* = ret[0..size];
+        } else {
+            self.allocs.appendAssumeCapacity(ret[0..size]);
+        }
+    }
+    return ret;
+}
+
+fn resize(ud: *anyopaque, slice: []u8, al: Alignment, new_len: usize, ret_addr: usize) bool {
+    const self: *CountingAllocator = @ptrCast(@alignCast(ud));
+    if (self.parent.rawResize(slice, al, new_len, ret_addr)) {
+        self.total = (self.total - slice.len) + new_len;
+        if (self.count_largest) {
+            self.findBlock(@intFromPtr(slice.ptr)).?.*.?.len = new_len;
+        }
+    }
+    return false;
+}
+
+fn remap(ud: *anyopaque, slice: []u8, al: Alignment, new_len: usize, ret_addr: usize) ?[*]u8 {
+    const self: *CountingAllocator = @ptrCast(@alignCast(ud));
+    const new_addr = self.parent.rawRemap(slice, al, new_len, ret_addr) orelse return null;
+    if (self.count_largest) {
+        self.findBlock(@intFromPtr(slice.ptr)).?.*.? = new_addr[0..new_len];
+    }
+    return new_addr;
+}
+
+fn free(ud: *anyopaque, slice: []u8, al: Alignment, ret_addr: usize) void {
+    const self: *CountingAllocator = @ptrCast(@alignCast(ud));
+    self.parent.rawFree(slice, al, ret_addr);
+    self.total -= slice.len;
+    if (self.count_largest) {
+        self.findBlock(@intFromPtr(slice.ptr)).?.* = null;
+        var i = self.allocs.items.len;
+        while (i > 0) : (i -= 1) {
+            const index = i - 1;
+            if (self.allocs.items[index] != null) {
+                self.allocs.items.len = i;
+                break;
+            }
+        }
+    }
+}
+
+parent: Allocator,
+total: usize,
+count_largest: bool,
+allocs: std.ArrayList(?[]u8),
+
+pub fn init(parent: Allocator, count_largest: bool) CountingAllocator {
+    return .{
+        .parent = parent,
+        .total = 0,
+        .count_largest = count_largest,
+        .allocs = .empty,
+    };
+}
+
+pub fn deinit(self: *CountingAllocator) void {
+    self.allocs.deinit(self.parent);
+}
+
+pub fn allocator(self: *CountingAllocator) Allocator {
+    return .{
+        .ptr = self,
+        .vtable = &vtable,
+    };
+}
+
+pub fn largestAllocation(self: *const CountingAllocator) usize {
+    std.debug.assert(self.count_largest);
+
+    var largest: usize = 0;
+    for (self.allocs.items) |may_block| {
+        if (may_block == null) continue;
+        largest = @max(largest, may_block.?.len);
+    }
+
+    return largest;
+}
+
+pub fn format(self: *const CountingAllocator, writer: *std.Io.Writer) std.Io.Writer.Error!void {
+    try writer.print("(total: {Bi}", .{self.total});
+    if (self.count_largest) {
+        try writer.writeAll(", slots:\n");
+
+        for (self.allocs.items, 0..) |ptr, i| {
+            if (ptr == null) continue;
+            try writer.writeAll("  ");
+            try writer.print("{Bi} [{d}%]: {x}", .{
+                ptr.?.len, (ptr.?.len * 100) / self.total,
+                ptr.?,
+            });
+            if (i + 1 < self.allocs.items.len) {
+                try writer.writeAll(",\n");
+            }
+        }
+    }
+    try writer.writeByte(')');
+}
